4b3aa591657e537588da87ed281a07fe5246ce95
jakub-kuba <jakub.kuba.piech@gmail.com>
2024-12-14 23:01:02 +0100

    autopep8 added

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "This is my project"}
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
